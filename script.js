2844b7071e1b68fd2ddb713b83d441179f03d967
treshombreskitchen <treshombreskitchen@gmail.com>
2025-08-13 21:57:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
   // }
 
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  toggleBtn.addEventListener('click', function () {
+    navLinks.classList.toggle('show');
+  });
+});
